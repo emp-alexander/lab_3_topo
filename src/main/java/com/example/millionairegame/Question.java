29690718caf13a746468f4e7a3f101f0f8b93643
Alexander <borisov2261@gmail.com>
2023-04-05 16:37:18 +0700

    Added constructor with param. Question with parameters test successes

--- src/main/java/com/example/millionairegame/Question.java
+++ src/main/java/com/example/millionairegame/Question.java
@@ -1,5 +1,21 @@
 package com.example.millionairegame;
 
+import java.util.List;
+
 public class Question {
+    private String quastionsText;
+    private int id;
+    private List<String> answerOptions;
+    private int correctAnswerIndex;
+
+    private int[] hint50;
+
+    public Question(String quastionsText, int id, List<String> answerOptions, int correctAnswerIndex, int[] hint50) {
+        this.quastionsText = quastionsText;
+        this.id = id;
+        this.answerOptions = answerOptions;
+        this.correctAnswerIndex = correctAnswerIndex;
+        this.hint50 = hint50;
+    }
     public Question(){};
 }
