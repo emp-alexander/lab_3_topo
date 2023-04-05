1d76a6018b2608f3757e5ccab86e831188eef883
Alexander <borisov2261@gmail.com>
2023-04-05 13:16:36 +0700

    Question creation test

---
+++ src/main/java/com/example/millionairegame/test/QuestionTest.java
@@ -0,0 +1,11 @@
+package com.example.millionairegame.test;
+
+import org.junit.Test;
+
+public class QuestionTest {
+
+    @Test
+    public void questionCreationTest(){
+        Question question = new Question();
+    }
+}
