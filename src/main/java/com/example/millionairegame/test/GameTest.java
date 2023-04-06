2000fd30a522549e19f70adb7c74a87e60022292
Alexander <borisov2261@gmail.com>
2023-04-06 12:12:38 +0700

    Game with parameters creation test

--- src/main/java/com/example/millionairegame/test/GameTest.java
+++ src/main/java/com/example/millionairegame/test/GameTest.java
@@ -2,13 +2,26 @@ package com.example.millionairegame.test;
 
 import com.example.millionairegame.Game;
 
+import com.example.millionairegame.Question;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 
 public class GameTest {
 
+
     @Test
     public void gameClassCreationTest(){
         Game game = new Game();
+    }
+
+    @Test
+    public void gameWithParamCreationTest(){
+        Game game = new Game(questions);
     }
 }
