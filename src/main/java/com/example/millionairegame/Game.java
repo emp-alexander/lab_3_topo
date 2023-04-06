0be67365cf5db92dcc1b6c4ff2fa4e15b2cb4141
Alexander <borisov2261@gmail.com>
2023-04-06 12:16:53 +0700

    Added constructor with param for Game. Game with parameters creation test successes

--- src/main/java/com/example/millionairegame/Game.java
+++ src/main/java/com/example/millionairegame/Game.java
@@ -1,5 +1,20 @@
 package com.example.millionairegame;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    private int score;
+    private ArrayList<Question> questions;
+    private int currentQuestionIndex;
+    private boolean[] hint;
+
     public Game(){};
+
+    public Game(ArrayList<Question> questions) {
+        score = 0;
+        this.questions = questions;
+        currentQuestionIndex = 0;
+        hint = new boolean[]{true, true, true};
+    }
 }
--- src/main/java/com/example/millionairegame/test/GameTest.java
+++ src/main/java/com/example/millionairegame/test/GameTest.java
@@ -14,6 +14,22 @@ import java.util.List;
 
 public class GameTest {
 
+    private Question q1, q2; //вопросы
+    private ArrayList<Question> questions; //пул вопросов
+    private List<String> answer1 = new ArrayList<>(); //вар-ты ответов к первому вопросу
+    private List<String> answer2 = new ArrayList<>(); //вар-ты ответов к второму вопросу
+    private int[] hint501, hint502; //подсказки 50/50 к 1 и 2 вопросу
+    @Before
+    public void setUp() throws Exception {
+        hint501 = new int[]{3, 2};
+        hint502 = new int[]{4, 2};
+        Collections.addAll(answer1, "тесто", "золото", "сено", "каша");
+        Collections.addAll(answer2, "Питсбург", "Москва", "Санкт-Петербург", "Киров");
+        q1 = new Question("Из чего сделан колобок?", 0,  answer1, 0, hint501);
+        q2 = new Question("Культурная столица России?", 1,  answer2, 3, hint502);
+        questions = new ArrayList<>();
+        Collections.addAll(questions, q1, q2);
+    }
 
     @Test
     public void gameClassCreationTest(){
